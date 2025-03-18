6002f34faa2539d9dced8c23c74e23c7f9342e0e
Oliver Ceccopieri <oliverbiotech@gmail.com>
2025-03-18 17:13:44 -0400

    Reorganize some functions

--- simple65.py
+++ simple65.py
@@ -18,14 +18,6 @@ instruction_list = []
 prog_counter = 0
 
 
-# Print an error message and quit
-def error(file, line, message):
-
-    print('ERROR!')
-    print('%s (%d): %s' % (file, line, message))
-    exit(-1)
-
-
 # Class to store information about a single opcode and its byte representations
 class Opcode:
 
@@ -49,194 +41,6 @@ class Opcode:
     def __getitem__(self, name):
 
         return getattr(self, name)
-
-
-# Table relating opcodes to their valid modes and byte representations
-opcode_table = {
-
-    # Math instructions
-    'ADC': Opcode(imm = 0x69, zp = 0x65, zp_x = 0x75, abs = 0x6D, abs_x = 0x7D, abs_y = 0x79, ind_x = 0x61, ind_y = 0x71),
-    'AND': Opcode(imm = 0x29, zp = 0x25, zp_x = 0x35, abs = 0x2D, abs_x = 0x3D, abs_y = 0x39, ind_x = 0x21, ind_y = 0x31),
-    'ASL': Opcode(implied = 0x0A, zp = 0x06, zp_x = 0x16, abs = 0x0E, abs_x = 0x1E),
-    'EOR': Opcode(imm = 0x49, zp = 0x45, zp_x = 0x55, abs = 0x4D, abs_x = 0x5D, abs_y = 0x59, ind_x = 0x41, ind_y = 0x51),
-    'LSR': Opcode(implied = 0x4A, zp = 0x46, zp_x = 0x56, abs = 0x4E, abs_x = 0x5E),
-    'ORA': Opcode(imm = 0x09, zp = 0x05, zp_x = 0x15, abs = 0x0D, abs_x = 0x1D, abs_y = 0x19, ind_x = 0x01, ind_y = 0x11),
-    'ROL': Opcode(implied = 0x2A, zp = 0x26, zp_x = 0x36, abs = 0x2E, abs_x = 0x3E),
-    'ROR': Opcode(implied = 0x6A, zp = 0x66, zp_x = 0x76, abs = 0x6E, abs_x = 0x7E),
-    'SBC': Opcode(imm = 0xE9, zp = 0xE5, zp_x = 0xF5, abs = 0xED, abs_x = 0xFD, abs_y = 0xF9, ind_x = 0xE1, ind_y = 0xF1),
-
-    # Memory instructions
-    'DEC': Opcode(zp = 0xC6, zp_x = 0xD6, abs = 0xCE, abs_x = 0xDE),
-    'INC': Opcode(zp = 0xE6, zp_x = 0xF6, abs = 0xEE, abs_x = 0xFE),
-    'LDA': Opcode(imm = 0xA9, zp = 0xA5, zp_x = 0xB5, abs = 0xAD, abs_x = 0xBD, abs_y = 0xB9, ind_x = 0xA1, ind_y = 0xB1),
-    'LDX': Opcode(imm = 0xA2, zp = 0xA6, zp_y = 0xB6, abs = 0xAE, abs_y = 0xBE),
-    'LDY': Opcode(imm = 0xA0, zp = 0xA4, zp_x = 0xB4, abs = 0xAC, abs_x = 0xBC),
-    'STA': Opcode(zp = 0x85, zp_x = 0x95, abs = 0x8D, abs_x = 0x9D, abs_y = 0x99, ind_x = 0x81, ind_y = 0x91),
-    'STX': Opcode(zp = 0x86, zp_y = 0x96, abs = 0x8E),
-    'STY': Opcode(zp = 0x84, zp_x = 0x94, abs = 0x8C),
-
-    # Comparison instructions
-    'BIT': Opcode(zp = 0x24, abs = 0x2C),
-    'CMP': Opcode(imm = 0xC9, zp = 0xC5, zp_x = 0xD5, abs = 0xCD, abs_x = 0xDD, abs_y = 0xD9, ind_x = 0xC1, ind_y = 0xD1),
-    'CPX': Opcode(imm = 0xE0, zp = 0xE4, abs = 0xEC),
-    'CPY': Opcode(imm = 0xC0, zp = 0xC4, abs = 0xCC),
-
-    # Branch instructions
-    'BCC': Opcode(off = 0x90),
-    'BCS': Opcode(off = 0xB0),
-    'BEQ': Opcode(off = 0xF0),
-    'BMI': Opcode(off = 0x30),
-    'BNE': Opcode(off = 0xD0),
-    'BPL': Opcode(off = 0x10),
-    'BVC': Opcode(off = 0x50),
-    'BVS': Opcode(off = 0x70),
-
-    # Control flow instructions
-    'JMP': Opcode(abs = 0x4C, ind = 0x6C),
-    'JSR': Opcode(abs = 0x20),
-    'RTI': Opcode(0x40),
-    'RTS': Opcode(0x60),
-
-    # Register instructions
-    'DEX': Opcode(0xCA),
-    'DEY': Opcode(0x88),
-    'INX': Opcode(0xE8),
-    'INY': Opcode(0xC8),
-    'TAX': Opcode(0xAA),
-    'TAY': Opcode(0xA8),
-    'TXA': Opcode(0x8A),
-    'TYA': Opcode(0x98),
-
-    # Stack instructions
-    'PHA': Opcode(0x48),
-    'PHP': Opcode(0x08),
-    'PLA': Opcode(0x68),
-    'PLP': Opcode(0x28),
-    'TSX': Opcode(0xBA),
-    'TXS': Opcode(0x9A),
-
-    # Flag instructions
-    'CLC': Opcode(0x18),
-    'CLD': Opcode(0xD8),
-    'CLI': Opcode(0x58),
-    'CLV': Opcode(0xB8),
-    'SEC': Opcode(0x38),
-    'SED': Opcode(0xF8),
-    'SEI': Opcode(0x78),
-
-    # Misc instructions
-    'BRK': Opcode(0x00),
-    'NOP': Opcode(0xEA),
-}
-
-
-# Directive to set the program counter
-def org_directive(param):
-
-    global prog_counter
-
-    new_pc = Value(param).literal
-    if new_pc is not None:
-        prog_counter = new_pc
-    else:
-        raise TypeError()
-
-
-# Directive to pad ROM with zeros
-def pad_directive(param):
-
-    global prog_counter
-
-    new_pc = Value(param).literal
-    if new_pc is not None:
-        while prog_counter < new_pc:
-            instruction_list.append(Value(0, length = 1))
-            prog_counter += 1
-    else:
-        raise TypeError()
-
-
-# Directive to output data bytes
-def db_directive(param):
-
-    global prog_counter
-
-    params = param_split_regex.split(param)
-    prog_counter += len(params)
-    instruction_list.extend(Value(p, length = 1) for p in params)
-
-
-# Directive to output data words
-def dw_directive(param):
-
-    global prog_counter
-
-    params = param_split_regex.split(param)
-    prog_counter += len(params)
-    instruction_list.extend(Value(p, length = 2) for p in params)
-
-
-# Directive to define a constant
-def def_directive(param):
-
-    params = param_split_regex.split(param)
-    name = params[0]
-
-    if name not in label_table:
-        val = Value(params[1]).literal
-        if val is not None:
-            label_table[name] = val
-        else:
-            raise TypeError()
-    else:
-        error(current_file, current_line, 'Label already exists "%s"' % name)
-
-
-# Directive to reserve bytes by incrementing the program counter, but without outputting bytes to ROM
-def rs_directive(param):
-
-    global prog_counter
-
-    new_pc = Value(param).literal
-    if new_pc is not None:
-        prog_counter += new_pc
-    else:
-        raise TypeError()
-
-
-# Directive to include an ASM file
-def include_directive(param):
-
-    parse_file(param.strip('\'"'))
-
-
-# Directive to include a file as binary
-def incbin_directive(param):
-
-    global prog_counter
-
-    file_name = param.strip('\'"')
-    try:
-        with open(file_name, 'rb') as bin_file:
-            bytes = bin_file.read()
-    except FileNotFoundError:
-        error(current_file, current_line, 'Binary file not found "%s"' % file_name)
-
-    instruction_list.extend(Value(b, length = 1) for b in bytes)
-    prog_counter += len(bytes)
-
-
-# Table relating directive names to their functions
-directive_table = {
-    '.ORG': org_directive,
-    '.PAD': pad_directive,
-    '.DB': db_directive,
-    '.DW': dw_directive,
-    '.DEF': def_directive,
-    '.RS': rs_directive,
-    '.INCLUDE': include_directive,
-    '.INCBIN': incbin_directive,
-}
 
 
 # Class for storing a value which can be used as an operand or binary data
@@ -420,6 +224,202 @@ class Instruction:
 
         out_bytes.extend(self.operand.get_bytes())
         return out_bytes
+
+
+# Print an error message and quit
+def error(file, line, message):
+
+    print('ERROR!')
+    print('%s (%d): %s' % (file, line, message))
+    exit(-1)
+
+
+# Directive to set the program counter
+def org_directive(param):
+
+    global prog_counter
+
+    new_pc = Value(param).literal
+    if new_pc is not None:
+        prog_counter = new_pc
+    else:
+        raise TypeError()
+
+
+# Directive to pad ROM with zeros
+def pad_directive(param):
+
+    global prog_counter
+
+    new_pc = Value(param).literal
+    if new_pc is not None:
+        while prog_counter < new_pc:
+            instruction_list.append(Value(0, length = 1))
+            prog_counter += 1
+    else:
+        raise TypeError()
+
+
+# Directive to output data bytes
+def db_directive(param):
+
+    global prog_counter
+
+    params = param_split_regex.split(param)
+    prog_counter += len(params)
+    instruction_list.extend(Value(p, length = 1) for p in params)
+
+
+# Directive to output data words
+def dw_directive(param):
+
+    global prog_counter
+
+    params = param_split_regex.split(param)
+    prog_counter += len(params)
+    instruction_list.extend(Value(p, length = 2) for p in params)
+
+
+# Directive to define a constant
+def def_directive(param):
+
+    params = param_split_regex.split(param)
+    name = params[0]
+
+    if name not in label_table:
+        val = Value(params[1]).literal
+        if val is not None:
+            label_table[name] = val
+        else:
+            raise TypeError()
+    else:
+        error(current_file, current_line, 'Label already exists "%s"' % name)
+
+
+# Directive to reserve bytes by incrementing the program counter, but without outputting bytes to ROM
+def rs_directive(param):
+
+    global prog_counter
+
+    new_pc = Value(param).literal
+    if new_pc is not None:
+        prog_counter += new_pc
+    else:
+        raise TypeError()
+
+
+# Directive to include an ASM file
+def include_directive(param):
+
+    parse_file(param.strip('\'"'))
+
+
+# Directive to include a file as binary
+def incbin_directive(param):
+
+    global prog_counter
+
+    file_name = param.strip('\'"')
+    try:
+        with open(file_name, 'rb') as bin_file:
+            bytes = bin_file.read()
+    except FileNotFoundError:
+        error(current_file, current_line, 'Binary file not found "%s"' % file_name)
+
+    instruction_list.extend(Value(b, length = 1) for b in bytes)
+    prog_counter += len(bytes)
+
+
+# Table relating directive names to their functions
+directive_table = {
+    '.ORG': org_directive,
+    '.PAD': pad_directive,
+    '.DB': db_directive,
+    '.DW': dw_directive,
+    '.DEF': def_directive,
+    '.RS': rs_directive,
+    '.INCLUDE': include_directive,
+    '.INCBIN': incbin_directive,
+}
+
+
+# Table relating opcodes to their valid modes and byte representations
+opcode_table = {
+
+    # Math instructions
+    'ADC': Opcode(imm = 0x69, zp = 0x65, zp_x = 0x75, abs = 0x6D, abs_x = 0x7D, abs_y = 0x79, ind_x = 0x61, ind_y = 0x71),
+    'AND': Opcode(imm = 0x29, zp = 0x25, zp_x = 0x35, abs = 0x2D, abs_x = 0x3D, abs_y = 0x39, ind_x = 0x21, ind_y = 0x31),
+    'ASL': Opcode(implied = 0x0A, zp = 0x06, zp_x = 0x16, abs = 0x0E, abs_x = 0x1E),
+    'EOR': Opcode(imm = 0x49, zp = 0x45, zp_x = 0x55, abs = 0x4D, abs_x = 0x5D, abs_y = 0x59, ind_x = 0x41, ind_y = 0x51),
+    'LSR': Opcode(implied = 0x4A, zp = 0x46, zp_x = 0x56, abs = 0x4E, abs_x = 0x5E),
+    'ORA': Opcode(imm = 0x09, zp = 0x05, zp_x = 0x15, abs = 0x0D, abs_x = 0x1D, abs_y = 0x19, ind_x = 0x01, ind_y = 0x11),
+    'ROL': Opcode(implied = 0x2A, zp = 0x26, zp_x = 0x36, abs = 0x2E, abs_x = 0x3E),
+    'ROR': Opcode(implied = 0x6A, zp = 0x66, zp_x = 0x76, abs = 0x6E, abs_x = 0x7E),
+    'SBC': Opcode(imm = 0xE9, zp = 0xE5, zp_x = 0xF5, abs = 0xED, abs_x = 0xFD, abs_y = 0xF9, ind_x = 0xE1, ind_y = 0xF1),
+
+    # Memory instructions
+    'DEC': Opcode(zp = 0xC6, zp_x = 0xD6, abs = 0xCE, abs_x = 0xDE),
+    'INC': Opcode(zp = 0xE6, zp_x = 0xF6, abs = 0xEE, abs_x = 0xFE),
+    'LDA': Opcode(imm = 0xA9, zp = 0xA5, zp_x = 0xB5, abs = 0xAD, abs_x = 0xBD, abs_y = 0xB9, ind_x = 0xA1, ind_y = 0xB1),
+    'LDX': Opcode(imm = 0xA2, zp = 0xA6, zp_y = 0xB6, abs = 0xAE, abs_y = 0xBE),
+    'LDY': Opcode(imm = 0xA0, zp = 0xA4, zp_x = 0xB4, abs = 0xAC, abs_x = 0xBC),
+    'STA': Opcode(zp = 0x85, zp_x = 0x95, abs = 0x8D, abs_x = 0x9D, abs_y = 0x99, ind_x = 0x81, ind_y = 0x91),
+    'STX': Opcode(zp = 0x86, zp_y = 0x96, abs = 0x8E),
+    'STY': Opcode(zp = 0x84, zp_x = 0x94, abs = 0x8C),
+
+    # Comparison instructions
+    'BIT': Opcode(zp = 0x24, abs = 0x2C),
+    'CMP': Opcode(imm = 0xC9, zp = 0xC5, zp_x = 0xD5, abs = 0xCD, abs_x = 0xDD, abs_y = 0xD9, ind_x = 0xC1, ind_y = 0xD1),
+    'CPX': Opcode(imm = 0xE0, zp = 0xE4, abs = 0xEC),
+    'CPY': Opcode(imm = 0xC0, zp = 0xC4, abs = 0xCC),
+
+    # Branch instructions
+    'BCC': Opcode(off = 0x90),
+    'BCS': Opcode(off = 0xB0),
+    'BEQ': Opcode(off = 0xF0),
+    'BMI': Opcode(off = 0x30),
+    'BNE': Opcode(off = 0xD0),
+    'BPL': Opcode(off = 0x10),
+    'BVC': Opcode(off = 0x50),
+    'BVS': Opcode(off = 0x70),
+
+    # Control flow instructions
+    'JMP': Opcode(abs = 0x4C, ind = 0x6C),
+    'JSR': Opcode(abs = 0x20),
+    'RTI': Opcode(0x40),
+    'RTS': Opcode(0x60),
+
+    # Register instructions
+    'DEX': Opcode(0xCA),
+    'DEY': Opcode(0x88),
+    'INX': Opcode(0xE8),
+    'INY': Opcode(0xC8),
+    'TAX': Opcode(0xAA),
+    'TAY': Opcode(0xA8),
+    'TXA': Opcode(0x8A),
+    'TYA': Opcode(0x98),
+
+    # Stack instructions
+    'PHA': Opcode(0x48),
+    'PHP': Opcode(0x08),
+    'PLA': Opcode(0x68),
+    'PLP': Opcode(0x28),
+    'TSX': Opcode(0xBA),
+    'TXS': Opcode(0x9A),
+
+    # Flag instructions
+    'CLC': Opcode(0x18),
+    'CLD': Opcode(0xD8),
+    'CLI': Opcode(0x58),
+    'CLV': Opcode(0xB8),
+    'SEC': Opcode(0x38),
+    'SED': Opcode(0xF8),
+    'SEI': Opcode(0x78),
+
+    # Misc instructions
+    'BRK': Opcode(0x00),
+    'NOP': Opcode(0xEA),
+}
 
 
 # Parse a line of ASM code
